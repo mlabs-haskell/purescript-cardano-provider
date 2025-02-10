5c45f7def0a29b6653945e107be1b7c738d571d8
Kristián Balaj <kiko.balaj@gmail.com>
2025-02-10 20:21:03 +0100

    feat: add server config helper fns

--- src/Provider/ServerConfig.purs
+++ src/Provider/ServerConfig.purs
@@ -1,10 +1,16 @@
 module Cardano.Provider.ServerConfig
   ( Host
   , ServerConfig
+  , mkHttpUrl
+  , mkServerUrl
+  , mkWsUrl
   ) where
 
-import Data.Maybe (Maybe)
+import Data.Maybe (Maybe, fromMaybe)
+import Data.Monoid ((<>))
+import Data.String (Pattern(..), null, stripPrefix, stripSuffix)
 import Data.UInt (UInt)
+import Data.UInt as UInt
 
 type Host = String
 
@@ -15,3 +21,30 @@ type ServerConfig =
   , path :: Maybe String
   }
 
+mkHttpUrl :: ServerConfig -> String
+mkHttpUrl = mkServerUrl "http"
+
+mkWsUrl :: ServerConfig -> String
+mkWsUrl = mkServerUrl "ws"
+
+mkServerUrl :: String -> ServerConfig -> String
+mkServerUrl protocol cfg =
+  (if cfg.secure then (protocol <> "s") else protocol)
+    <> "://"
+    <> cfg.host
+    <> ":"
+    <> UInt.toString cfg.port
+    <</>> fromMaybe "" cfg.path
+
+-- | Concat two strings with "/" in the middle, but stripping multiple slashes.
+-- No slash if second string empty.
+concatPaths :: String -> String -> String
+concatPaths a b =
+  if null right then left
+  else left <> "/" <> right
+
+  where
+  left = fromMaybe a (stripSuffix (Pattern "/") a)
+  right = fromMaybe b (stripPrefix (Pattern "/") b)
+
+infixr 5 concatPaths as <</>> -- </> is taken
